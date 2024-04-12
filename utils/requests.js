c830b9956d7c8f0a1c349225221151d8f57fd3c3
Johnny Kaze <thisiskazejohnny@gmail.com>
2024-04-12 04:57:17 -0700

    refactor requests.js with bt final version

--- utils/requests.js
+++ utils/requests.js
@@ -1,26 +1,16 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
-/* 
-So the way this works is we're going to create an API route to use mongoose to get the properties from the database.
-
-And then from our front end right from this component, we're going to then fetch from our API route and then display the properties.
-**/
-
 // Fetch all properties
 async function fetchProperties({ showFeatured = false } = {}) {
   try {
-    //Handle the case where the domain is not available yet
+    // Handle the case where the domain is not available yet
     if (!apiDomain) {
       return []
     }
 
-    console.log(apiDomain)
-
     const res = await fetch(
-      `${apiDomain}/properties/${showFeatured ? 'featured' : ''}`,
-      {
-        cache: 'no-store',
-      }
+      `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+      { cache: 'no-store' }
     )
 
     if (!res.ok) {
@@ -37,7 +27,7 @@ async function fetchProperties({ showFeatured = false } = {}) {
 // Fetch single property
 async function fetchProperty(id) {
   try {
-    //Handle the case where the domain is not available yet
+    // Handle the case where the domain is not available yet
     if (!apiDomain) {
       return null
     }
@@ -50,7 +40,7 @@ async function fetchProperty(id) {
 
     return res.json()
   } catch (error) {
-    // console.log(error)
+    console.log(error)
     return null
   }
 }
