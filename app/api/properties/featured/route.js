84a84462a0cedecfe71533358c740003316ac2bc
Johnny Kaze <thisiskazejohnny@gmail.com>
2024-04-12 07:43:34 -0700

    refactor

--- app/api/properties/featured/route.js
+++ app/api/properties/featured/route.js
@@ -18,6 +18,6 @@ export const GET = async (request) => {
     return Response.json(properties)
   } catch (error) {
     console.log(error)
-    return new Response('Something Went Wrong', { status: 500 })
+    return new Response('Something went wrong', { status: 500 })
   }
 }
--- utils/requests.js
+++ utils/requests.js
@@ -9,7 +9,7 @@ async function fetchProperties({ showFeatured = false } = {}) {
     }
 
     const res = await fetch(
-      `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+      `${apiDomain}/properties${showFeatured ? '/featured' : '/'}`,
       { cache: 'no-store' }
     )
 
