3e24d84856fb574a8398fcb22dfd82c196730f71
Johnny Kaze <thisiskazejohnny@gmail.com>
2024-04-12 07:47:16 -0700

    update mongo connection string

--- config/database.js
+++ config/database.js
@@ -12,7 +12,10 @@ const connectDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      dbName: 'propertypulse',
+    })
     connected = 'true'
     console.log('MongoDB connected...')
   } catch (error) {
